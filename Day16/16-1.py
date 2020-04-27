ed0be53499a43a52f49fa6d988d212b70b7b8549
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-04-27 16:58:17 -0400

    Day16_partial

---
+++ Day16/16-1.py
@@ -0,0 +1,26 @@
+import re
+
+sues = [{attributes.split()[0].rstrip(':'): int(attributes.split()[1]) for attributes in re.split('^Sue [0-9]+: ', line.rstrip())[1].split(', ')} for line in open("input.txt", "r")]
+
+gifter = {
+    "children": 3,
+    "cats": 7,
+    "samoyeds": 2,
+    "pomeranians": 3,
+    "akitas": 0,
+    "vizslas": 0,
+    "goldfish": 5,
+    "trees": 3,
+    "cars": 2,
+    "perfumes": 1
+}
+
+for (index, sue) in enumerate(sues):
+    foundSue = True
+    for label in sue.keys():
+        if sue.get(label) != gifter.get(label):
+            foundSue = False
+            break
+    
+    if foundSue:
+        print("Gifter is Sue ", index + 1)
