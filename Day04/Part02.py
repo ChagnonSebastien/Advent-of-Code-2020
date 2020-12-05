766e523e879788457b227d4940f87d260038ca36
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-12-05 13:55:45 -0500

    Day04 cleanup

--- Day04/Part02.py
+++ Day04/Part02.py
@@ -1,14 +1,13 @@
 from re import search
 
 def isValid(p):
-    return (
-        search(r'byr:19[2-9][0-9]|200[0-2]', p) and 
-        search(r'iyr:201[0-9]|2020', p) and
-        search(r'eyr:202[0-9]|2030', p) and
-        search(r'hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)', p) and
-        search(r'hcl:#[0-9a-f]{6}(?![0-9a-f])', p) and
-        search(r'(ecl:(amb|blu|brn|gry|grn|hzl|oth))', p) and
-        search(r'pid:[0-9]{9}(?![0-9])', p)
+    return (search(r'byr:19[2-9][0-9]|200[0-2]', p)
+        and search(r'iyr:201[0-9]|2020', p)
+        and search(r'eyr:202[0-9]|2030', p)
+        and search(r'hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)', p)
+        and search(r'hcl:#[0-9a-f]{6}(?![0-9a-f])', p)
+        and search(r'(ecl:(amb|blu|brn|gry|grn|hzl|oth))', p)
+        and search(r'pid:[0-9]{9}(?![0-9])', p)
     )
 
 lines = [line for line in open("Day04/input", "r").read().split("\n")]
