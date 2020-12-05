8f6d41447299b0253f92e81c50f35d2aa41fb0b7
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-12-05 13:55:35 -0500

    Day03 cleanup

--- Day03/Part01.py
+++ Day03/Part01.py
@@ -1,9 +1,3 @@
 lines = [line for line in open("Day03/input", "r").read().strip().split("\n")]
-hits = 0
-offset = 0
-for line in lines:
-  if line[offset] == "#":
-    hits += 1
-  offset += 3
-  offset %= len(line)
+hits = sum([line[3 * i % len(line)] == "#" for i, line in enumerate(lines)])
 print(hits)
--- Day03/Part02.py
+++ Day03/Part02.py
@@ -1,13 +1,6 @@
+from math import prod
+
 lines = [line for line in open("Day03/input", "r").read().strip().split("\n")]
-hits = 1
-for right, down in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
-  offset = 0
-  slopeHits = 0
-  for i, line in enumerate(lines):
-    if i % down == 0:
-      if line[offset] == "#":
-        slopeHits += 1
-      offset += right
-      offset %= len(line)
-  hits *= slopeHits
-print(hits)
+def hits(right, down): return sum([lines[j][right * i % len(lines[j])] == "#" for i, j in enumerate(range(0, len(lines), down))])
+result = prod([hits(right, down) for right, down in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]])
+print(result)
