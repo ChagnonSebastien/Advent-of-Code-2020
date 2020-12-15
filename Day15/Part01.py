cb11fdd9338de51d922da8cccf9cb37be6272732
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-12-15 00:41:25 -0500

    Day15

---
+++ Day15/Part01.py
@@ -0,0 +1,17 @@
+numbers = open("Day15/input", "r").read().strip().split(',')
+lastOccurence = { int(n): (i + 1) for i, n in enumerate(numbers[:len(numbers) - 1]) }
+round = len(numbers) + 1
+last = int(numbers[len(numbers) - 1])
+
+while round <= 2020:
+    if last not in lastOccurence.keys():
+        lastOccurence.update({ last: round - 1 })
+        last = 0
+    else:
+        prev = lastOccurence.get(last)
+        lastOccurence.update({ last: round - 1 })
+        last = round - prev - 1
+
+    round += 1
+
+print(last)
